2f95205ccfded09a160c3131ef697309f076abdf
Uchenna F. Okoye <okoyefaithful@gmail.com>
2015-06-03 10:35:10 -0500

    Initial Commit

---
+++ My_Task_List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My_Task_List
+//
+//  Created by Faithful Okoye on 6/3/15.
+//  Copyright (c) 2015 Uchenna Okoye IOS Design. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
